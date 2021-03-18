89d3772879d76a2069e66d49d181d846778008e5
Razvan Bugariu <razvanbugariu@pop-os.localdomain>
2021-03-18 15:01:32 +0200

    eventbus are working

---
+++ eventbus.demo/src/main/java/reactive/clusters/MainVerticle.java
@@ -0,0 +1,21 @@
+package reactive.clusters;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.core.AbstractVerticle;
+
+public class MainVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() {
+        vertx.eventBus().<String>consumer("hello", message -> {
+            JsonObject json = new JsonObject()
+                    .put("served-by", this.toString());
+            if (message.body().isEmpty()) {
+                message.reply(json.put("message", "hello"));
+            } else {
+                message.reply(json.put("message", "hello " + message.body()));
+            }
+        });
+    }
+
+}
